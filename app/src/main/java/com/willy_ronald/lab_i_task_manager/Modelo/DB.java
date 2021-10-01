95ff60151a6f00c0f0773f9b0e159bd83270b233
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-10-01 13:37:04 -0600

    Initial commit

---
+++ app/src/main/java/com/willy_ronald/lab_i_task_manager/Modelo/DB.java
@@ -0,0 +1,4 @@
+package com.willy_ronald.lab_i_task_manager.Modelo;
+
+public class DB{
+}
---
+++ app/src/main/java/com/willy_ronald/lab_i_task_manager/act_modificar_tarea.java
@@ -0,0 +1,14 @@
+package com.willy_ronald.lab_i_task_manager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_modificar_tarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lyt_modificar_tarea);
+    }
+}
