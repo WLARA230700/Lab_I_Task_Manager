62efae2fe0b6b5156ee4ddbbff01e66b28b37965
RonaldCh578 <70775974+RonaldCh578@users.noreply.github.com>
2021-10-01 15:08:38 -0600

    frontend - Página principal

--- app/src/main/java/com/willy_ronald/lab_i_task_manager/act_main.java
+++ app/src/main/java/com/willy_ronald/lab_i_task_manager/act_main.java
@@ -3,12 +3,33 @@ package com.willy_ronald.lab_i_task_manager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class act_main extends AppCompatActivity {
+
+    Spinner spinnerCategorias;
+
+    String[] categorias = {"Trabajo", "Doméstica", "Universidad"};
+
+    ArrayAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lyt_main);
+        spinnerCategorias = findViewById(R.id.spinnerCategorias);
+
+        asignarCategoriasSpinner();
+
+    }//Fin onCreate
+
+    public void asignarCategoriasSpinner(){
+        ArrayList<String> categoriasList = new ArrayList<>(Arrays.asList(categorias));
+        adapter = new ArrayAdapter(getApplicationContext(), android.R.layout.simple_list_item_1,categoriasList);
+        spinnerCategorias.setAdapter(adapter);
     }
 }
